be84f15973dbda4848050d106c950dd9ee1452c8
unknown <s.selvaganesh2005@gmail.com>
2026-01-30 14:41:26 +0530

    0.2

--- hello.py
+++ hello.py
@@ -1,11 +1,12 @@
 def addnum(a, b):
     return a+b
-
+def sub(a,b):
+    return a-b
 n = int(input("enter 1.addition 2.subraction : "))
 
 if(n==1):
     result = addnum(int(input("enter a: ")), int(input("enter b: ")))
     print(f"addtion of a b = {result}")
 elif(n==2):
-    result = 0 #function subraction
-    print(f"addtion of a b = {result}")
+    result = sub(int(input("enter the a value:")), int(input("enter hte b value:")))
+    print(f"subtraction of a b = {result}")
